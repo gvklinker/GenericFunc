8050747c899b594019f723f2215151ac175e18e3
Gustav Vang Klinker <guk001@edu.zealand.dk>
2024-02-07 09:34:36 +0100

    Add project files.

---
+++ GenericFunc/Car.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericFunc
+{
+    public class Car
+    {
+        public int Id { get; set; }
+        public string Regno { get; set; }
+        public string Make { get; set; }
+
+        public int Speed { get; set; }
+
+        private int _counter = 0;
+
+        public Car(string regno, string make, int speed)
+        {
+            Id = _counter++;
+            Regno = regno;
+            Make = make;
+            Speed = speed;
+
+        }
+
+        public override string ToString()
+        {
+            return $"{{{nameof(Id)}={Id.ToString()}, {nameof(Regno)}={Regno}, {nameof(Make)}={Make}, {nameof(Speed)}={Speed.ToString()}}}";
+        }
+
+    }
+
+}

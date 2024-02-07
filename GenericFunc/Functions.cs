cd49b217250a17411cbb730f17efd9ebd118fca4
Gustav Vang Klinker <guk001@edu.zealand.dk>
2024-02-07 10:05:16 +0100

    p

---
+++ GenericFunc/Functions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericFunc
+{
+    public class Functions
+    {
+        public static int FindLargest(List<int> list)
+        {
+            int result = 0;
+            foreach(int i in list)
+            {
+                if(i > result)
+                    result = i;
+            }
+            return result;
+        }
+    }
+}
